a7456f93b8ec433bfc94ad924a9a0bc389cacca0
Maksym Ushakov <ushakov.m@hotmail.com>
2021-03-09 16:52:08 +0200

    feat: add math functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
-exports.min = function min (array) {
+exports.min = function min (array) {  
+  if (Array.isArray(array) && array.length) {
+    return Math.min(...array);
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (Array.isArray(array) && array.length) {
+    return Math.max(...array);
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (Array.isArray(array) && array.length) {
+    return array.reduce( (acc, el) => acc + el, 0) / array.length;
+  }
   return 0;
 }
